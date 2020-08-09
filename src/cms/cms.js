1662a47e021dbd2e184e2c7568892b318aace18d
Tanner Fiscus <tanner@tannerfiscus.com>
2020-08-09 15:55:04 -0400

    Fix broken import

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,16 +3,12 @@ import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 import MenuPagePreview from './preview-templates/MenuPagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
-CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('menu', MenuPagePreview)
