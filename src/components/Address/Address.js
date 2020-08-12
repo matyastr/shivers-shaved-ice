6ab3731068569c176d94ab88d4ff0de8edce58d8
Tanner Fiscus <tanner@tannerfiscus.com>
2020-08-12 10:18:04 -0400

    Render hours and address from template, remove blog from cms

--- src/components/Address/Address.js
+++ src/components/Address/Address.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import addressData from '../../templates/data/address.json';
+
 const Address = () => (
     <>
-        1234 Location TBD<br />
-        Gibsonia, Pennsylvania
+        {
+            addressData.address.map(({ line }, i) => (
+                <>
+                    <span>{ line }</span>
+                    { i < (addressData.address.length - 1) && <br />}
+                </>
+            ))
+        }
     </>
 );
 
